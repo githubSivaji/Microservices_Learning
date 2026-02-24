7cf6224f8c4de62c24f077630172b637140e737a
githubSivaji <123107544+githubSivaji@users.noreply.github.com>
2026-02-24 14:33:50 +0530

    cards microservice created and done

--- cards/src/main/java/com/microservices/cards/CardsApplication.java
+++ cards/src/main/java/com/microservices/cards/CardsApplication.java
@@ -3,11 +3,43 @@ package com.microservices.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.microservices.cards.controller") })
+@EnableJpaRepositories("com.microservices.cards.repository")
+@EntityScan("com.microservices.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Bank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "sivaji",
+						email = "tutor@eazybytes.com",
+						url = "https://www.eazybytes.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://sivaji.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApplication.class, args);
 	}
-
 }
